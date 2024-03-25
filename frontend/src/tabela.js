d5a6063e43c73b4eb96e357577bf39707a7b5640
Jose A F Santos <toni.farias@outlook.com>
2024-03-24 21:11:25 -0300

    atualização do componente tabela pre CSS

--- frontend/src/tabela.js
+++ frontend/src/tabela.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 function Tabela() {
     const [dados, setDados] = useState([]);
     const [dataSelecionada, setDataSelecionada] = useState('');
+    const [registrosPorPagina, setRegistrosPorPagina] = useState(15);
     const [paginaAtual, setPaginaAtual] = useState(1);
 
     const handleDataChange = (event) => {
@@ -21,29 +22,29 @@ function Tabela() {
                 throw new Error('Erro ao buscar destinatários');
             }
             const destinatarios = await response.json(); // Receber a lista de destinatários
-            const conteudoEmails = comporConteudoEmail(dados); // Compor o conteúdo do e-mail com base nos resultados da busca
-            enviarEmails(destinatarios, conteudoEmails); // Enviar e-mails com o conteúdo composto
+            const conteudoEmail = comporConteudoEmail(dados); // Compor o conteúdo do e-mail com base nos resultados da busca
+            enviarEmails(destinatarios, conteudoEmail); // Enviar e-mails com o conteúdo composto
         } catch (error) {
             console.error('Erro ao buscar destinatários:', error);
         }
     };
 
     const comporConteudoEmail = (dados) => {
-        return dados.map(item => {
-            let saudacao = `<p>Olá ${item.cliente.split(' ')[0]},</p>`;
-            let assinatura = '';
+        let conteudo = '';
+        dados.forEach(item => {
+            conteudo += `<p>Olá ${item.cliente.split(' ')[0]},</p>`; // Obter o primeiro nome do cliente
             if (item.cnpj) {
-                assinatura = `<p>Sua assinatura do tipo ${item.produto} em nome de ${item.razaoSocial} vencerá dia ${item.dataVencimento}, entre em contato conosco para realizarmos a renovação. Basta responder esse e-mail ou nos ligar nos números...</p>`;
+                conteudo += `<p>Sua assinatura do tipo ${item.produto} em nome de ${item.razaoSocial} vencerá dia ${item.dataVencimento}, entre em contato conosco para realizarmos a renovação. Basta responder esse e-mail ou nos ligar nos números...</p>`;
             } else {
-                assinatura = `<p>Sua assinatura do tipo ${item.produto} em nome de ${item.cliente} vencerá dia ${item.dataVencimento}, entre em contato conosco para realizarmos a renovação. Basta responder esse e-mail ou nos ligar nos números...</p>`;
+                conteudo += `<p>Sua assinatura do tipo ${item.produto} em nome de ${item.cliente} vencerá dia ${item.dataVencimento}, entre em contato conosco para realizarmos a renovação. Basta responder esse e-mail ou nos ligar nos números...</p>`;
             }
-            return saudacao + assinatura;
         });
+        return conteudo;
     };
 
-    const enviarEmails = async (destinatarios, conteudoEmails) => {
+    const enviarEmails = async (destinatarios, conteudo) => {
         try {
-            destinatarios.forEach(async (destinatario, index) => {
+            destinatarios.forEach(async (destinatario) => {
                 const response = await fetch('http://localhost:8080/enviar-email', {
                     method: 'POST',
                     headers: {
@@ -52,7 +53,7 @@ function Tabela() {
                     body: JSON.stringify({
                         destinatarios: [destinatario],
                         assunto: 'Assunto do e-mail',
-                        conteudo: conteudoEmails[index] // Usar o conteúdo do e-mail composto
+                        conteudo: conteudo // Usar o conteúdo do e-mail composto
                     })
                 });
 
@@ -61,7 +62,7 @@ function Tabela() {
                     throw new Error(`Erro ao enviar e-mail para ${destinatario}: ${errorMessage}`);
                 }
 
-                console.log(`E-mail enviado com sucesso.`);
+                console.log(`E-mail enviado com sucesso para ${destinatario}.`);
             });
         } catch (error) {
             console.error('Erro ao enviar e-mails:', error.message);
@@ -74,7 +75,7 @@ function Tabela() {
 
     const fetchDados = async () => {
         try {
-            const response = await fetch(`http://localhost:8080/listar-data?dataVencimento=${dataSelecionada}&pagina=${paginaAtual}`);
+            const response = await fetch(`http://localhost:8080/listar-data?dataVencimento=${dataSelecionada}&pagina=${paginaAtual}&registrosPorPagina=${registrosPorPagina}`);
             if (!response.ok) {
                 throw new Error('Erro ao carregar dados do servidor');
             }
@@ -90,12 +91,13 @@ function Tabela() {
             <h1>Tabela de Dados</h1>
             <label htmlFor="dataSelecionada">Selecione uma data:</label>
             <input type="text" id="dataSelecionada" value={dataSelecionada} onChange={handleDataChange} />
-            <table>
+            <table className='table'>
                 <thead>
                     <tr>
                         <th>Pedido</th>
                         <th>Data de Vencimento</th>
                         <th>Cliente</th>
+                        <th>Razão Social</th>
                         <th>Email</th>
                         <th>Telefone</th>
                         <th>Produto</th>
@@ -109,6 +111,7 @@ function Tabela() {
                             <td>{item.pedido}</td>
                             <td>{item.dataVencimento}</td>
                             <td>{item.cliente}</td>
+                            <td>{item.razaoSocial}</td>
                             <td>{item.email}</td>
                             <td>{item.telefone}</td>
                             <td>{item.produto}</td>
