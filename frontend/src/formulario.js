060c48affb1c8880aa589eeed3fd0da908da4509
Jose A F Santos <toni.farias@outlook.com>
2024-04-06 21:22:03 -0300

    Observações no formulário de cadastro

--- frontend/src/formulario.js
+++ frontend/src/formulario.js
@@ -79,37 +79,40 @@ function Formulario() {
       <h2>Formulário de Cadastro</h2>
       <form onSubmit={handleSubmit} style={{ display: 'flex', justifyContent: 'space-between' }}>
         <div style={{ width: '50%', textAlign: 'left', marginRight: '20px', marginLeft: '20px'}}>
-          <label>Pedido:</label>
-          <input type="text" value={pedido} onChange={(e) => setPedido(e.target.value)} required className='form-control'/><br />
+          <label>Pedido*:</label>
+          <input placeholder='Somente números' type="text" value={pedido} onChange={(e) => setPedido(e.target.value)} required className='form-control'/><br />
 
-          <label>Cliente:</label>
-          <input type="text" value={cliente} onChange={(e) => setCliente(e.target.value)} required className='form-control'/><br />
+          <label>Cliente*:</label>
+          <input placeholder='Nome do cliente' type="text" value={cliente} onChange={(e) => setCliente(e.target.value)} required className='form-control'/><br />
           
-          <label>CPF:</label>
-          <input type="text" value={cpf} onChange={(e) => setCPF(e.target.value)} required className='form-control'/><br />
+          <label>CPF*:</label>
+          <input  placeholder='Somente números' type="text" value={cpf} onChange={(e) => setCPF(e.target.value)} required className='form-control'/><br />
 
-          <label>Email:</label>
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required className='form-control'/><br />
+          <label>E-mail*:</label>
+          <input placeholder='email@do.cliente' type="email" value={email} onChange={(e) => setEmail(e.target.value)} required className='form-control'/><br />
 
-          <label>Telefone:</label>
-          <input type="text" value={telefone} onChange={(e) => setTelefone(e.target.value)} required className='form-control'/><br />
+          <label>Telefone*:</label>
+          <input placeholder='Telefone com DDD somente números' type="text" value={telefone} onChange={(e) => setTelefone(e.target.value)} required className='form-control'/><br />
         </div>
         <div style={{ width: '50%', textAlign: 'left' }}>
-          <label>Produto:</label>
-          <input type="text" value={produto} onChange={(e) => setProduto(e.target.value)} required className='form-control'/><br />
+          <label>Produto*:</label>
+          <input placeholder='Tipo do certificado' type="text" value={produto} onChange={(e) => setProduto(e.target.value)} required className='form-control'/><br />
 
-          <label>CNPJ:</label>
-          <input type="text" value={cnpj} onChange={(e) => setCNPJ(e.target.value)} className='form-control'/><br />
+          <label>CNPJ**:</label>
+          <input placeholder='Somente números' type="text" value={cnpj} onChange={(e) => setCNPJ(e.target.value)} className='form-control'/><br />
 
-          <label>Razão Social:</label>
-          <input type="text" value={razaoSocial} onChange={(e) => setRazaoSocial(e.target.value)} className='form-control'/><br />
+          <label>Razão Social**:</label>
+          <input placeholder='Razão Social' type="text" value={razaoSocial} onChange={(e) => setRazaoSocial(e.target.value)} className='form-control'/><br />
 
-          <label>Data de Vencimento:</label>
-          <input type="text" value={dataVencimento} onChange={(e) => setDataVencimento(e.target.value)} required className='form-control'/><br />
+          <label>Data de Vencimento*:</label>
+          <input placeholder='00/00/00' type="text" value={dataVencimento} onChange={(e) => setDataVencimento(e.target.value)} required className='form-control'/><br />
 
           <div style={{ display: 'flex', justifyContent: 'left', width: '100%'}}>
           <button type="submit" style={{ marginRight: '10px', marginTop: '24px' }} className='btn btn-success'>Enviar</button><br />
           <button type="button" onClick={handleLimparFormulario} style={{ marginLeft: '10px', marginTop: '24px' }}className='btn btn-secondary'>Limpar Formulário</button>
+          </div>
+          <div style={{color: 'gray', fontSize: 12}}>
+            <p>*: Campo obrigatório. **: CNPJ e Razão Social devem ser preenchidos, um dos campos não pode ser vazio.</p>
           </div>
         </div>
       </form>
